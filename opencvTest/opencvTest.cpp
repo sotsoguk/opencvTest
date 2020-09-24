143711dd1d8a8c0021d599bd05205fed9536f727
Bert Riffelmacher <sotsoguk@gmail.com>
2020-09-24 11:00:31 +0200

    load image

--- opencvTest/opencvTest.cpp
+++ opencvTest/opencvTest.cpp
@@ -2,6 +2,7 @@
 //
 
 #include <iostream>
+#include <filesystem>
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc.hpp>
@@ -10,9 +11,12 @@ using namespace std;
 using namespace cv;
 int main()
 {
-	Mat image = Mat::zeros(300, 600, CV_8UC3);
-	circle(image, Point(250, 150), 100, Scalar(0, 255, 128), -100);
-	circle(image, Point(350, 150), 100, Scalar(255, 255, 255), -100);
+	filesystem::path cwd = filesystem::current_path();
+	cout << cwd << endl;
+	//Mat image = Mat::zeros(300, 600, CV_8UC3);
+	Mat image = imread("img1.jpg");
+	//circle(image, Point(250, 150), 100, Scalar(0, 255, 128), -100);
+	//circle(image, Point(350, 150), 100, Scalar(255, 255, 255), -100);
 	imshow("Display Window", image);
 	waitKey(0);
 	return 0;
